0a4ba86332fee05e82035e5bcd36c18055fb9ac1
Iaisidndwkoedkkskd <118977000+MADNESSTEST@users.noreply.github.com>
2022-12-06 22:41:28 +0800

    Create yessir.lua

---
+++ yessir.lua
@@ -0,0 +1,11 @@
+return {
+["Axl_20009"] = true,
+["GuestGuestGuest919"] = true,
+["robloxguestgirl555"] = true,
+["noobplayeronly727"] = true,
+["mecoolboi83"] = true,
+["JxcExploiETs"] = true,
+["pinnedstraw"] = true,
+["jshojshs"] = true,
+["robloxguestboy555"] = true -- put "," in end if you want to make another one execpt the last one
+}
